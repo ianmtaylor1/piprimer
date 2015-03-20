02ce0bb585cc729fef3fa8fbefb6f991fda179ec
Ian Taylor <ian.m.taylor1@gmail.com>
2015-03-20 00:34:35 -0400

    Made counting speed configurable

--- piprimer.py
+++ piprimer.py
@@ -15,6 +15,8 @@ pinmode = GPIO.BCM
 outputpins = [4,17,27,22,10,9,11,14,15,18,23,24,25,8,7]
 # Pin which is connected to the button, with a PULL UP resistor
 buttonpin = 2
+# Speed at which the primer should count (numbers/second)
+speed = 5
 
 # Set up the GPIO pins and binary counter
 GPIO.setmode(pinmode)
@@ -40,10 +42,11 @@ value = start
 def buttonwait():
     global value
     global c
+    global speed
     while True:
         GPIO.wait_for_edge(buttonpin,GPIO.FALLING)
         with counterlock:
-            y = countprime(value+1,c,0.2)
+            y = countprime(value+1,c,1.0/speed)
             print('{s} {d} {e}'.format(s=value,d='.'*(y-value-1),e=y))
             value = y
 
@@ -51,10 +54,11 @@ def buttonwait():
 def keyboardwait():
     global value
     global c
+    global speed
     while True:
         input()
         with counterlock:
-            y = countprime(value+1,c,0.2)
+            y = countprime(value+1,c,1.0/speed)
             print('{s} {d} {e}'.format(s=value,d='.'*(y-value-1),e=y))
             value = y
 
