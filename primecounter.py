50bd081a646bd6b8eaccf531e34365950b9f5404
Ian Taylor <ian.m.taylor1@gmail.com>
2015-03-19 20:57:02 -0400

    Moved main file to piprimer.py and counting functions to primecounter.py

--- primecounter.py
+++ primecounter.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-
-# Program to count up numbers and identify primes
 from math import sqrt
 from time import sleep
-import RPi.GPIO as GPIO
-from binaryledcounter import BinaryLEDCounter
-from sys import argv
-from random import randrange
-
-# List of output pins
-# The i'th element of this array represents the 2^i bit of our display
-outputpins = [4,17,27,22,10,9,11,14,15,18,23,24,25,8,7]
-buttonpin = 2
+#from binaryledcounter import BinaryLEDCounter
 
 def isprime(x):
     """Determine if x is prime."""
@@ -45,26 +34,4 @@ def countprime(start,counter,delay):
             sleep(delay)
     return x
 
-# Set up the GPIO pins and binary counter
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-c = BinaryLEDCounter(outputpins)
-GPIO.setup(buttonpin,GPIO.IN)
 
-# Find out where we should start
-if len(argv)==1:
-    start = c.getvalue()
-elif (argv[1]=='random')or(argv[1]=='rand'):
-    start = randrange(0,c.maxvalue()+1)
-else:
-    start = int(argv[1])
-c.setvalue(start)
-print("Start: {}".format(start))
-
-# Loop infinitely, counting to a new prime every time user hits enter
-x = start
-while True:
-    GPIO.wait_for_edge(buttonpin,GPIO.FALLING)
-    y = countprime(x+1,c,0.2)
-    print('{s} {d} {e}'.format(s=x,d='.'*(y-x-1),e=y))
-    x = y
